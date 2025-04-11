f7db1c5bf7ceb19479b5164a3825e36c70fe2c60
Manvi Grover <manvi0862.be21@chitkara.edu.in>
2025-04-11 10:56:03 +0530

    [MANVI GROVER] ADD: Created json object.

--- src/main/java/com/bridgelabz/practice/CreateJsonObject.java
+++ src/main/java/com/bridgelabz/practice/CreateJsonObject.java
@@ -1,7 +1,21 @@
 package com.bridgelabz.practice;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class CreateJsonObject {
     public static void main(String[] args) {
+        JSONArray subjects=new JSONArray();
+        subjects.put("Physics");
+        subjects.put("Chemistry");
+        subjects.put("Maths");
+
+        JSONObject student=new JSONObject();
+        student.put("name","vashu");
+        student.put("age",17);
+        student.put("subjects",subjects);
+
+        System.out.println(student.toString(1));
 
     }
 }
